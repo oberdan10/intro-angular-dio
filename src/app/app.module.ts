cd0ad06783de95acc01f3ac519948799e5fd4865
Oberdan Rocha <oberdan.rbsantos@gmail.com>
2022-07-14 10:18:14 -0300

    NavBar, Rotas e Erro404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,38 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CursoListaComponent } from './cursos/curso-lista.component';
+import { Error404Component } from './error-404/error-404.component';
 import { EstrelasComponent } from './estrelas/estrelas.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CursoListaComponent,
-    EstrelasComponent
+    EstrelasComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    //ROTAS DE ACESSO
+    RouterModule.forRoot([
+      { 
+        path: '' ,redirectTo: 'cursos', pathMatch: 'full'
+      },
+      { 
+        path: 'cursos', component: CursoListaComponent
+      },
+      { 
+        path: '**', component: Error404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cursos/curso-lista.component.ts
+++ src/app/cursos/curso-lista.component.ts
@@ -1,71 +1,33 @@
 import { Component, OnInit } from "@angular/core";
 import { Curso } from "./curso";
+import { CursoService } from "./curso.service";
 
 @Component({
-    selector: 'app-curso-lista',
     templateUrl: './curso-lista.component.html'
-
 })
 export class CursoListaComponent implements OnInit {
- cursos: Curso[] = [];
+    _cursos: Curso[] = [];
+    cursosFiltrados: Curso[] = [];
+    _filtrarPor!: string;
+    
 
- ngOnInit(): void { //Inicialização de curso-lista
-    this.cursos = [
-        {
-            id: 1,
-            name: 'Angular: CLI',
-            releaseDate: 'November 2, 2019',
-            description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
-            duration: 120,
-            code: 'XLF-1212',
-            rating: 3,
-            price: 12.99,
-            imageUrl: '/assets/images/cli.png',
-        },
-        {
-            id: 2,
-            name: 'Angular: Forms',
-            releaseDate: 'November 4, 2019',
-            description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Forms.',
-            duration: 80,
-            code: 'DWQ-3412',
-            rating: 3.5,
-            price: 24.99,
-            imageUrl: '/assets/images/forms.png',
-        },
-        {
-            id: 3,
-            name: 'Angular: HTTP',
-            releaseDate: 'November 8, 2019',
-            description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de HTTP.',
-            duration: 80,
-            code: 'QPL-0913',
-            rating: 4.0,
-            price: 36.99,
-            imageUrl: '/assets/images/http.png',
-        },
-        {
-            id: 4,
-            name: 'Angular: Router',
-            releaseDate: 'November 16, 2019',
-            description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Router.',
-            duration: 80,
-            code: 'OHP-1095',
-            rating: 4.5,
-            price: 46.99,
-            imageUrl: '/assets/images/router.png',
-        },
-        {
-            id: 5,
-            name: 'Angular: Animations',
-            releaseDate: 'November 25, 2019',
-            description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis sobre Animation.',
-            duration: 80,
-            code: 'PWY-9381',
-            rating: 5,
-            price: 56.99,
-            imageUrl: '/assets/images/animations.png',
-        }
-    ];
- }
+    //Injeção de Dependência - Curso Serviço
+    constructor(private cursoService: CursoService) {
+
+    }
+
+    ngOnInit(): void { //Inicialização de curso-lista
+        this._cursos = this.cursoService.retrieveAll();
+        this.cursosFiltrados = this._cursos;
+
+    }
+
+    set filtrar(value: string) {
+        this._filtrarPor = value;
+        this.cursosFiltrados = this._cursos.filter((curso: Curso) => curso.name.toLocaleLowerCase().indexOf(this._filtrarPor.toLocaleLowerCase()) > -1)
+    }
+
+    get filtrar() {
+        return this._filtrarPor;
+    }
 }
